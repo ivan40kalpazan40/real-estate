4ee40919ead70b047c304bd5da10a64df511ec51
Ivan Skordev <maymunskoa@yahoo.com>
2021-10-14 18:19:54 +0300

    link href fixed to properly connect with css source

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-require('./config/handlebars')(app);
 
+require('./config/handlebars')(app);
+app.use(express.static(path.resolve(__dirname, './public')));
 app.get('/', (req, res) => {
   res.render('index');
 });
