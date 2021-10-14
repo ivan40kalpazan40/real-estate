f32a4065b40e5bfd8baba3ffef4fce46a8a0f2cd
Ivan Skordev <maymunskoa@yahoo.com>
2021-10-14 23:59:25 +0300

    connect to mongodb

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const path = require('path');
 const routes = require('./routes');
+const { connectionString, PORT } = require('./config/utils');
+const initDb = require('./config/db');
 const app = express();
 
 require('./config/handlebars')(app);
 app.use(express.static(path.resolve(__dirname, './public')));
 app.use(routes);
-app.listen(5000, () => {
-  console.log('Server is listening on port 5000....');
-});
+initDb(connectionString())
+  .then(() => {
+    console.log('Connected to DB');
+    app.listen(PORT(), () => {
+      console.log('Server is listening on port 5000....');
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
---
+++ src/config/utils.js
@@ -0,0 +1,2 @@
+exports.connectionString = () => 'mongodb://localhost:27017/housing';
+exports.PORT = () => 5000;
