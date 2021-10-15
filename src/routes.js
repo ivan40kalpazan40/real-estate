f6152cdc4702b6f96def29c5f4c38f706aa59247
Ivan Skordev <maymunskoa@yahoo.com>
2021-10-15 11:56:49 +0300

    create housing controller and render all housings page

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const homeController = require('./controllers/homeController');
 const userController = require('./controllers/userController');
+const housingController = require('./controllers/housingController');
 const router = express.Router();
 
 router.use(homeController);
 router.use('/user', userController);
+router.use('/housing', housingController);
 module.exports = router;
